20421c03ce2253d262d8b598da43202db53d1047
Corentin Lallier <corentin.lallier@mindflow.io>
2023-12-26 19:02:47 +0100

    feat: update project to be runnable again

--- js/main.js
+++ js/main.js
@@ -7,41 +7,42 @@ var RetroWipe = window.RetroWipe || {};
 
 
 RetroWipe.init = function () {
-    'use strict';
+  'use strict';
 
-    // on initialise le moteur de rendu
-    RetroWipe.renderer = new THREE.WebGLRenderer();
+  // on initialise le moteur de rendu
+  RetroWipe.renderer = new THREE.WebGLRenderer();
 
-    // si WebGL ne fonctionne pas sur votre navigateur vous pouvez utiliser le moteur de rendu Canvas à la place
-    // renderer = new THREE.CanvasRenderer();
-    RetroWipe.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('container').appendChild(RetroWipe.renderer.domElement);
+  // si WebGL ne fonctionne pas sur votre navigateur vous pouvez utiliser le moteur de rendu Canvas à la place
+  // renderer = new THREE.CanvasRenderer();
+  RetroWipe.renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('container').appendChild(RetroWipe.renderer.domElement);
 
-    // on initialise la scène
-    RetroWipe.scene = new THREE.Scene();
+  // on initialise la scène
+  RetroWipe.scene = new THREE.Scene();
 
-    // on initialise la camera que l’on place ensuite sur la scène
-    RetroWipe.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-    RetroWipe.camera.position.set(0, 0, 100);
-    RetroWipe.scene.add(RetroWipe.camera);
+  // on initialise la camera que l’on place ensuite sur la scène
+  RetroWipe.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+  RetroWipe.camera.position.set(0, 0, 300);
+  RetroWipe.scene.add(RetroWipe.camera);
 
-    //control camera
-    //RetroWipe.control = new OrbitControls(RetroWipe.camera, RetroWipe.renderer.domElement);
+  //control camera
+  //RetroWipe.control = new OrbitControls(RetroWipe.camera, RetroWipe.renderer.domElement);
 
-    // ligths
-    var ambientLight = new THREE.AmbientLight(0x555555);
-    RetroWipe.scene.add(ambientLight);
+  // ligths
+  var ambientLight = new THREE.AmbientLight(0x555555);
+  RetroWipe.scene.add(ambientLight);
 
-    // mesh loader
-    RetroWipe.loader = new THREE.JSONLoader();
-    //RetroWipe.loader.load( "data/ship01.js", RetroWipe.AddModelToScene, "data/ship01_layout_2_green.png");
-    RetroWipe.loader.load( "data/jamjar.json", RetroWipe.AddModelToScene, "data/jamjar_layout.png");
+  // mesh loader
+  RetroWipe.loader = new THREE.JSONLoader();
+  RetroWipe.loader.load("data/ship01.js", RetroWipe.AddModelToScene, "data/ship01_layout_2_green.png");
+  //RetroWipe.loader.load( "data/jamjar.json", RetroWipe.AddModelToScene, "data/jamjar_layout.png");
 };
 
 RetroWipe.AddModelToScene = function (geometry) {
   'use strict';
   geometry.computeTangents();
-  let material = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( "data/jamjar_layout.png" )});
+  let material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture("data/ship01_layout_2_green.png") });
+  //let material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture("data/jamjar_layout.png") });
   material.map.magFilter = THREE.NearestFilter;
   material.map.minFilter = THREE.NearestFilter;
   RetroWipe.mesh = new THREE.Mesh(geometry, material);
@@ -58,29 +59,29 @@ RetroWipe.AddModelToScene = function (geometry) {
 };
 
 RetroWipe.animate = function () {
-    'use strict';
-    // on appel la fonction animate() récursivement à chaque frame
-    window.requestAnimationFrame(RetroWipe.animate);
-    // on fait tourner le cube sur ses axes x et y
-    RetroWipe.mesh.rotation.y += 0.01;
-    RetroWipe.mesh.updateMatrix();
+  'use strict';
+  // on appel la fonction animate() récursivement à chaque frame
+  window.requestAnimationFrame(RetroWipe.animate);
+  // on fait tourner le cube sur ses axes x et y
+  RetroWipe.mesh.rotation.y += 0.01;
+  RetroWipe.mesh.updateMatrix();
 
-    if(RetroWipe.control != null)
-      RetroWipe.control.update();
+  if (RetroWipe.control != null)
+    RetroWipe.control.update();
 
-    // on effectue le rendu de la scène
-    RetroWipe.renderer.render(RetroWipe.scene, RetroWipe.camera);
+  // on effectue le rendu de la scène
+  RetroWipe.renderer.render(RetroWipe.scene, RetroWipe.camera);
 };
 
 RetroWipe.music = function () {
-    // http://www.nullsleep.com/treasure/mck_guide/
+  // http://www.nullsleep.com/treasure/mck_guide/
 
-    'use strict';
-    //var mml1 = "l4 o5 [bagfedc<]2 o4[aa+bcc+dd+eff+gg+]4";
-    var mml1 = "A [c d e f g4 a16 b16 >c c d e f g4 a16 b16 >c<<]2";
-    mml1 += "C [c e g8 g8 a16 b16 >c8 c e g8 g8 a16 b16 >c8<<]4";
-    var gen = new T("PluckGen", {env: {type: "perc", r: 500, lv: 0.4}}).play();
-    var music_line = new T("mml", {mml: mml1}, gen).start();
+  'use strict';
+  //var mml1 = "l4 o5 [bagfedc<]2 o4[aa+bcc+dd+eff+gg+]4";
+  var mml1 = "A [c d e f g4 a16 b16 >c c d e f g4 a16 b16 >c<<]2";
+  mml1 += "C [c e g8 g8 a16 b16 >c8 c e g8 g8 a16 b16 >c8<<]4";
+  var gen = new T("PluckGen", { env: { type: "perc", r: 500, lv: 0.4 } }).play();
+  var music_line = new T("mml", { mml: mml1 }, gen).start();
 };
 
 
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Experimental Three.js and Blender Project
+
+## Overview
+
+This project is an experimental exploration of `three.js`, Blender and pixel art textures.
+
+### Features
+
+- **Three.js Integration**: Utilizes `three.js` for rendering 3D models and animations in a web environment. Warning: this use a deprecated version of `three.js` (0.70.1) in order to use the `JSONLoader` object (need to be replaced with a glTF model)
+- **Blender Models**: Incorporates sophisticated 3D models and scenes created and exported from Blender.
+- **Pixel Art Textures**: Features unique pixel art textures, giving the project a distinctive style and visual appeal.
+
+## Getting Started
+
+```bash
+# install dependencies
+yarn
+
+# build all
+yarn run build
+
+# run live-server
